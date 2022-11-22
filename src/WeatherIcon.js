619e91c4c74fddbb29e4b6a7185510394f7053a5
AnastasiiaKmit <anastasiiakmit99@gmail.com>
2022-11-22 18:46:18 +0000

    Changed the size of the icon and deleted default icon

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -27,7 +27,7 @@ export default function WeatherIcon(props) {
       <ReactAnimatedWeather
         icon={codeMapping[props.code]}
         color="#0B5ED7"
-        size="55"
+        size={65}
         animate={true}
       />
     </div>
